5565e1f97dbb99c53d157d8adfcacbb76581cfdd
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-09-13 01:44:40 -0700

    finished product

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Set up MySQL connection.
+require('dotenv').config()
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+ ('The Classic', FALSE),
+ ('Veggie', FALSE),
+ ('Supersized', FALSE),
+ ('Double-double', TRUE);
